d5b35df0b46d8d029bf5966ca9a19a284521af0b
paan <paan@ahmadfarhan.com>
2025-05-16 10:29:46 +0800

    synching tick data via the integration layer

--- src-tauri/src/integrations/ui.rs
+++ src-tauri/src/integrations/ui.rs
@@ -1,9 +1,11 @@
 use tauri::State;
 use std::sync::{atomic::{AtomicU64, Ordering}, Arc};
+use legion::system;
+use crate::sim::resources::global::TickCounter;
 
 
 #[derive(Clone, Debug, Default)]
-pub struct AppState {
+pub struct AppState {// this is tha main integration state
     pub tick: Arc<AtomicU64>,
 }
 
@@ -11,6 +13,16 @@ pub struct AppState {
 pub fn get_tick(state: State<'_, Arc<AppState>>) -> u64 {
     state.tick.load(Ordering::Relaxed)
 }
+
+
+
+
+#[system]
+pub fn update_tick(#[resource]tick_counter: &Arc<TickCounter>){
+    tick_counter.tick.fetch_add(1, Ordering::Relaxed);
+}
+
+
 
 pub fn update_appstate(){
     //this updates appstate
