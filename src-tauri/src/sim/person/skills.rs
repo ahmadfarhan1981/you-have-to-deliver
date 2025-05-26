b5dbc53e6db31cfe4c416a1d070a758d18303ada
paan <paan@ahmadfarhan.com>
2025-05-26 23:00:26 +0800

    minor

--- src-tauri/src/sim/person/skills.rs
+++ src-tauri/src/sim/person/skills.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::master_data::skills::SkillDef;
 use crate::sim::person::stats::StatType;
 use serde::{Deserialize, Serialize};
@@ -15,12 +16,43 @@ struct SkillBoost {
     direct_boost: Vec<SkillLink>,
     indirect_boost: Vec<SkillLink>,
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Tier {
+    Foundational,
+    Conceptual,
+    Applied,
+}
+
+impl std::fmt::Display for Tier {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            Tier::Foundational => "Foundational",
+            Tier::Conceptual => "Conceptual",
+            Tier::Applied => "Applied",
+        };
+        write!(f, "{}", name)
+    }
+}
+impl std::str::FromStr for Tier {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input.to_lowercase().as_str() {
+            "foundational" => Ok(Tier::Foundational),
+            "conceptual" => Ok(Tier::Conceptual),
+            "applied" => Ok(Tier::Applied),
+            _ => Err(()),
+        }
+    }
+}
+
 
 pub struct GlobalSkill {
     pub id: SkillId,
     pub slug: String,
     pub name: String,
     pub description: String,
+    pub tier: Tier,
     pub feedforward: Vec<SkillLink>,
     pub feedback: Vec<SkillLink>,
     pub related_stats: Vec<StatType>,
@@ -34,6 +66,7 @@ impl From<&SkillDef> for GlobalSkill {
             description: value.description.parse().unwrap(),
             feedforward: vec![],
             feedback: vec![],
+            tier: Tier::from_str(value.tier).unwrap(),
             related_stats: value.related_stats.iter().fold(Vec::new(), |mut acc, s| {
                 match s.parse::<StatType>() {
                     Ok(stat) => acc.push(stat),
