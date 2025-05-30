1ce202d020233ddbbc2b11f29845da402d3b1e80
farhan <ajaafar@guidewire.com>
2025-05-30 17:40:12 +0800

    more chart stuff

--- src/lib/components/StatPanel.svelte
+++ src/lib/components/StatPanel.svelte
@@ -8,28 +8,28 @@
 
 <div class="border border-slate-200 rounded-lg p-6 bg-white shadow-sm mb-6">
     <h3 class="text-lg font-bold mb-4">Skills & Attributes</h3>
-    <div class="h-[250px] w-[250px]"><StatsEchart statsSnapshot={stats} /></div>
-    {#each stats_definition as group}
-        <div class="mb-4 ">
+    <div class="h-[300px] w-[300px]"><StatsEchart statsSnapshot={stats} /></div>
+    <!--{#each stats_definition as group}-->
+    <!--    <div class="mb-4 ">-->
 
 
-            <h4 class="text-sm font-semibold text-slate-600 mb-2">{group.group}</h4>
-            <div class="space-y-4">
-                {#each group.items as item}
-                    <div class="mb-1">
-                        <div class="flex justify-between text-xs mb-1">
-                            <span>{item.label}</span>
-                            <span>{stats[item.key]}/100</span>
-                        </div>
-                        <div class="w-full bg-slate-200 rounded-full h-2">
-                            <div
-                                    class={`h-2 rounded-full ${item.class}`}
-                                    style={`width: ${stats[item.key]}%`}
-                            ></div>
-                        </div>
-                    </div>
-                {/each}
-            </div>
-        </div>
-    {/each}
+    <!--        <h4 class="text-sm font-semibold text-slate-600 mb-2">{group.group}</h4>-->
+    <!--        <div class="space-y-4">-->
+    <!--            {#each group.items as item}-->
+    <!--                <div class="mb-1">-->
+    <!--                    <div class="flex justify-between text-xs mb-1">-->
+    <!--                        <span>{item.label}</span>-->
+    <!--                        <span>{stats[item.key]}/100</span>-->
+    <!--                    </div>-->
+    <!--                    <div class="w-full bg-slate-200 rounded-full h-2">-->
+    <!--                        <div-->
+    <!--                                class={`h-2 rounded-full ${item.class}`}-->
+    <!--                                style={`width: ${stats[item.key]}%`}-->
+    <!--                        ></div>-->
+    <!--                    </div>-->
+    <!--                </div>-->
+    <!--            {/each}-->
+    <!--        </div>-->
+    <!--    </div>-->
+    <!--{/each}-->
 </div>
