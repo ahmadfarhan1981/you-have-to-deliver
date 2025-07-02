cb7745a15679ddad771c581c5185f261c2482db1
Ahmad Farhan <33049840+ahmadfarhan1981@users.noreply.github.com>
2025-07-02 14:54:32 +0800

    refactor load game handling

--- src-tauri/src/simulation_thread.rs
+++ src-tauri/src/simulation_thread.rs
@@ -9,8 +9,7 @@ use tracing::{error, info, trace};
 
 use crate::action_queues::sim_manager::{SimManager, SimManagerCommand};
 use crate::action_queues::team_manager::TeamManagerCommand;
-use crate::db::constants::{db_keys, save_version};
-use crate::db::init::{SaveSlot, SaveSlotMetadata, SavesDirectory};
+use crate::db::init::SavesDirectory;
 use crate::integrations::events::{emit_app_event, AppEventType};
 use crate::integrations::queues::SimCommand;
 use crate::integrations::queues::SimCommand::TeamManager;
@@ -18,10 +17,9 @@ use crate::schedules::init::GameSchedules;
 use crate::sim::action::action::ActionIntent;
 use crate::sim::calendar::components::CalendarEvent;
 use crate::sim::company::company::{Company, PlayerControlled};
-use crate::sim::persistence::persistence::{LoadGame, SavedEmployee};
+use crate::sim::persistence::persistence::{load_game_state, LoadGame, SavedEmployee};
 use crate::sim::person::init::ShouldGenerateEmployees;
 use crate::sim::resources::global::Dirty;
-use crate::sim::sim_date::sim_date::SimDate;
 use crate::sim::team::components::Team;
 use crate::sim::utils::debugging::DebugDisplayComponent;
 use crate::utils::errors::SavesManagementError;
@@ -182,10 +180,16 @@ pub fn run_simulation_thread(config: SimThreadConfig) {
             .execute(&mut world, &mut resources);
 
         if loop_sim_manager.is_running() {
-            if loop_load_game.should_load.load(Ordering::Relaxed) { 
-                if let Err(e) = handle_game_load(
-                    &mut world, &mut resources, &loop_load_game, &loop_tick_counter, &loop_sim_manager,
-                    &loop_snapshot_state, &loop_snapshot_registry, &mut game_schedules
+            if loop_load_game.should_load.load(Ordering::Relaxed) {
+                if let Err(e) = load_game_state(
+                    &mut world,
+                    &mut resources,
+                    &loop_load_game,
+                    &loop_tick_counter,
+                    &loop_sim_manager,
+                    &loop_snapshot_state,
+                    &loop_snapshot_registry,
+                    &mut game_schedules,
                 ) {
                     error!("Failed to load game: {:?}", e);
                 }
@@ -244,106 +248,4 @@ pub fn run_simulation_thread(config: SimThreadConfig) {
             sleeper.sleep(Duration::from_millis(100));
         }
     }
-}
-
-fn handle_game_load(
-    world: &mut World,
-    resources: &mut Resources,
-    loop_load_game: &Arc<LoadGame>,
-    loop_tick_counter: &Arc<TickCounter>,
-    loop_sim_manager: &Arc<SimManager>,
-    loop_snapshot_state: &Arc<SnapshotState>,
-    loop_snapshot_registry: &Arc<SnapshotEmitRegistry>,
-    game_schedules: &mut GameSchedules,
-) -> Result<(), SavesManagementError> {
-    //switch to save slot.
-    let saves_directory = resources.get::<Arc<SavesDirectory>>()
-        .ok_or_else(|| SavesManagementError::Io(io::Error::new(io::ErrorKind::NotFound, "SavesDirectory resource not found")))?
-        .clone();
-    let slot = loop_load_game.slot_id.read().as_ref()
-        .ok_or_else(|| SavesManagementError::Io(io::Error::new(io::ErrorKind::NotFound, "Save slot ID not set")))?
-        .clone();
-
-    let mut save_slot = SaveSlot {
-        slot_id: slot.clone(),
-        path: saves_directory.0.join(slot.clone()),
-        metadata: None,
-        is_empty: false,
-        handle: None,
-    };
-    
-    save_slot.ensure_db_handle_is_open(&saves_directory)?; // populates the handle
-    
-    info!("Resetting the world...");
-    world.clear();
-
-    info!("Loading employees...");
-    let employee_list = save_slot.load_entry::<Vec<u32>>(db_keys::EMPLOYEES_LIST)?;
-    let _number_of_employees = employee_list.len(); // Variable not used, can be removed or prefixed with _
-    for employee_id in employee_list {
-        let employee = save_slot.load_entry::<SavedEmployee>(format!("{}{}", db_keys::EMPLOYEE_PREFIX, employee_id).as_str())?;
-        
-        info!("Loading employees: {:?}", employee);
-        world.push((
-            employee.person,
-            employee.stats,
-            employee.profile_picture,
-            employee.personality_matrix,
-            employee.hunger,
-            employee.energy,
-            employee.skill_set,
-            employee.stress_level,
-            employee.current_goal,
-            DebugDisplayComponent::default(),
-            PlayerControlled,
-            Dirty
-        ));
-    }
-
-    info!("Loading teams...");
-    let teams = save_slot.load_entry::<Vec<Team>>(db_keys::TEAMS)?;
-    for team in teams {
-        world.push((team, Dirty));
-    }
-    
-    info!("Loading calendar events");
-    let calendar_events: Vec<CalendarEvent> =save_slot.load_entry::<Vec<CalendarEvent>>(db_keys::CALENDAR_EVENTS)?;
-    for calendar_event in calendar_events{
-        world.push((calendar_event, Dirty));
-    }
-
-    info!("Loading company...");
-    let company = save_slot.load_entry::<Company>(db_keys::COMPANY)?;
-    let _company_name = company.name.clone(); // Variable not used, can be removed or prefixed with _
-    world.push((company, PlayerControlled, Dirty));
-
-    info!("Loading tick_counter...");
-    let tick_counter = save_slot.load_entry::<TickCounter>(db_keys::TICK_COUNTER)?;
-    loop_tick_counter.update_from(&tick_counter);
-
-    let metadata = save_slot.load_entry::<SaveSlotMetadata>(db_keys::METADATA)?;
-    save_slot.metadata = Some(metadata);
-    
-    //load data
-    info!("Load game {:?}.", loop_load_game);
-    loop_load_game.should_load.store(false, Ordering::Relaxed);
-    let mut slot_write_guard = loop_load_game.slot_id.write();
-    *slot_write_guard = None;
-
-    *loop_sim_manager.save_slot.lock() = Some(save_slot);
-    
-    game_schedules.load_game_schedule.execute(world, resources); // repopulate registries
-
-    //clear snapshot state
-    loop_snapshot_state.reset();
-    //reset emitters
-    loop_snapshot_registry.reset();
-    
-    
-    let app_context = resources.get::<Arc<AppContext>>()
-        .ok_or_else(|| SavesManagementError::Io(io::Error::new(io::ErrorKind::NotFound, "AppContext resource not found")))?;
-    info!("emit_done_setup_event");
-    emit_app_event(&app_context.app_handle, AppEventType::InitDone);
-
-    Ok(())
 }
