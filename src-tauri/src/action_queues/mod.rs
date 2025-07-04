7b020430a45c1f4ec4802434f6c1811236275646
Ahmad Farhan <33049840+ahmadfarhan1981@users.noreply.github.com>
2025-07-04 09:11:15 +0800

    Add Thought command queue

--- src-tauri/src/action_queues/mod.rs
+++ src-tauri/src/action_queues/mod.rs
@@ -2,3 +2,4 @@ pub mod game_speed_manager;
 pub mod shared;
 pub mod sim_manager;
 pub mod team_manager;
+pub mod thought_manager;
---
+++ src-tauri/src/action_queues/thought_manager.rs
@@ -0,0 +1,43 @@
+use crate::action_queues::shared::timed_dispatch;
+use crate::integrations::queues::QueueManager;
+use crate::sim::person::thoughts::{ArchivedThoughts, Thought, Thoughts};
+use crate::sim::person::components::{PersonId};
+use crate::sim::registries::registry::Registry;
+use crate::sim::resources::global::Dirty;
+use legion::{system, world::SubWorld, systems::CommandBuffer, Entity, IntoQuery, Query};
+use std::sync::Arc;
+use std::time::Duration;
+use tracing::{trace, info, warn};
+
+pub enum ThoughtCommand {
+    AddThought { person_id: u32, thought: Thought },
+}
+
+#[system]
+pub fn handle_thought_command_queue(
+    #[resource] queue_manager: &QueueManager,
+    #[resource] person_registry: &Arc<Registry<PersonId, Entity>>,
+    thought_query: &mut Query<(&mut Thoughts, &mut ArchivedThoughts)>,
+    world: &mut SubWorld,
+    commands: &mut CommandBuffer,
+) {
+    trace!("Handling thought command queue");
+    let queue = &queue_manager.thought_manager;
+    let dispatch_time_limit = Duration::from_millis(5);
+
+    timed_dispatch(queue, dispatch_time_limit, |cmd| match cmd {
+        ThoughtCommand::AddThought { person_id, thought } => {
+            info!("Adding thought to person {}", person_id);
+            if let Some(entity) = person_registry.get_entity_from_id(&PersonId(person_id)) {
+                if let Ok((mut thoughts, mut archived)) = thought_query.get_mut(world, entity) {
+                    thoughts.add(thought, &mut archived);
+                    commands.add_component(entity, Dirty);
+                } else {
+                    warn!("Cannot access thought components for person {}", person_id);
+                }
+            } else {
+                warn!("Person id {} not found while adding thought", person_id);
+            }
+        }
+    });
+}
